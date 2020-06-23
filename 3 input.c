dafdf65997e3e969ac98d307380ff4ad9b2da869
XxSirilxX <67195990+XxSirilxX@users.noreply.github.com>
2020-06-23 22:00:43 +0530

    Add files via upload

---
+++ 3 input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d",&a,&b,&c);
+    d=a+b+c;
+    printf("Sum of %d+%d+%d = %d\n",a,b,c,d);
+    return 0;
+}
